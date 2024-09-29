c4e0d9335259d435264350c80fd065e6445b2e11
alireza <alireza2000jadidi@gmail.com>
2024-09-29 19:29:32 -0400

    added test cases and basic design for the python todo list

---
+++ cli_todo/test_todo.py
@@ -0,0 +1,17 @@
+import unittest
+import todo_list
+
+
+class TestTodo(unittest.TestCase):
+
+    def test_menu(self):
+        pass
+
+    def test_add(self):
+        pass
+
+    def test_delete(self):
+        pass
+
+    def test_view(self):
+        pass
--- cli_todo/todo_list.py
+++ cli_todo/todo_list.py
@@ -9,25 +9,39 @@ class Task:
         self.todo = todo
         self.done = done
 
-    def checkTask(self):
+    def check_task(self):
         self.done = not self.done
 
-    def getTask(self):
+    def get_printed_task(self):
         return f"[{'x' if self.done else ' '}] {self.todo}"
 
 
-def addTask(tasks: List[str], newTask: Task):
+class TaskInventory:
+    def __init__(self):
+        self.taskList = list()
 
-    pass
+    def add_task(self):
+        print("Enter the task details: ")
+        todo = input()
+        self.taskList.append(Task(todo, False))
+
+    def delete_task(self, task: Task):
+        if task in self.taskList:
+            self.taskList.remove(task)
+        else:
+            print("Task is not in inventory to remove ")
+
+    def view_tasks(self):
+        for task in self.taskList:
+            print(task.get_printed_task())
+
+    def menu(self):
+        opts = {"view the tasks", "delete a task", "add a task"}
+        print("What do you want to do?")
+        for i, opt in enumerate(opts):
+            print(f"{i+1}. {opt}")
 
 
-def deleteTask(tasks: List[str]):
-    pass
-
-
-def viewTasks(tasks: List[str]):
-    pass
-
-
-def menu():
-    pass
+if __name__ == "__main__":
+    inventroy = TaskInventory()
+    inventroy.menu()
