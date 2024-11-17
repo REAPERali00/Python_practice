9b31d579047a05f504b2b3b7b52da773caddc0e2
alireza <alireza2000jadidi@gmail.com>
2024-11-16 21:41:21 -0500

    read gc notes

--- German/main.py
+++ German/main.py
@@ -1,8 +1,32 @@
 from card import Card
+import re
 
 
-# with open("coffee_break.md", "r") as file:
-#     print(file.read())
+class Question:
+    def __init__(self, german: str, english: str):
+        self.german = german
+        self.english = english
+
+    def __repr__(self):
+        return f"Question(German: '{self.german}', English:'{self.english}')"
+
+
+class Game:
+    
+    def read_gc_data(self, filepath):
+        with open(filepath, "r", encoding="utf-8") as file:
+            notes = file.read()
+        pattern = r"- `([^`]+)`-> (.+)"
+        matches = re.findall(pattern, notes)
+        questions = [
+            Question(german.strip(), english.strip()) for german, english in matches
+        ]
+        return questions
+    def card_question(self ): 
+        ...
+
+
+
 Card(
     "DO you like it so much that you are willing to die for it? ", 20, 0.5
 ).print_card()
